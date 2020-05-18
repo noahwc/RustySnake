0fd180967dd2bee2a80bfe0aa2ad1139b732ae33
evan maier <evanm413r@gmail.com>
2020-05-17 23:58:52 -0700

    improved testing functionality

--- src/test.rs
+++ src/test.rs
@@ -2,6 +2,9 @@ use super::rocket;
 use crate::responses;
 use rocket::http::{ContentType, Status};
 use rocket::local::Client;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 #[test]
 fn ping() {
@@ -66,52 +69,21 @@ fn start() {
 
 #[test]
 fn movement() {
+    // set game id and turn here
+    let game_id = "db2e56ff-ead4-443b-9f60-902edeed5472";
+    let turn: usize = 16;
+
+    let mut f = File::open(format!("./logs/{}.txt", game_id)).expect("failed to open file");
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer).expect("failed to read file");
+    let turns: Vec<&str> = buffer.lines().collect();
+    let turn_data = turns[turn];
+
     let client = Client::new(rocket()).expect("Failed to create client instance");
     let mut response = client
         .post("/move")
         .header(ContentType::JSON)
-        .body(
-            r#"{
-            "game": {
-                "id": "game-id-string"
-            },
-            "turn": 4,
-            "board": {
-                "height": 15,
-                "width": 15,
-                "food": [
-                {
-                    "x": 2,
-                    "y": 3
-                }
-                ],
-                "snakes": [
-                    {
-                        "id": "snake-id-string",
-                        "name": "Sneky Snek",
-                        "health": 90,
-                        "body": [
-                            {
-                                "x": 1,
-                                "y": 3
-                            }
-                        ]
-                    }
-                ]
-            },
-            "you": {
-                "id": "snake-id-string",
-                "name": "Sneky Snek",
-                "health": 90,
-                "body": [
-                {
-                    "x": 1,
-                    "y": 3
-                }
-                ]
-            }
-        }"#,
-        )
+        .body(turn_data)
         .dispatch();
     assert_eq!(response.status(), Status::Ok);
     // test the response to match the regex
